fbbc49fde98f95f7c2534d61614db8f8eeb5226f
MarianaGallego <marianagallego97@hotmail.com>
2025-05-23 02:10:44 -0500

    Lee mensaje, lee y ajusta clave corta

--- Laboratorio_2.asm
+++ Laboratorio_2.asm
@@ -1,11 +1,14 @@
 .data
 
-	inputFile: .asciiz "C:/Users/maria/OneDrive - Universidad de Antioquia/Escritorio/Arquitectura/2025 - 1/Arquitectura - Laboratorio 2/input.txt"
+	inputFile: .asciiz "C:/Users/maria/OneDrive - Universidad de Antioquia/Escritorio/Arquitectura/2025 - 1/Arquitectura_Laboratorio 2/input.txt"
 	mensaje: .space 1024
 	longitudMensaje: .word 0
 	
 	textoClaveCorta: .asciiz "Ingrese la palabra que desea utilizar como clave corta:   "
 	claveCorta: .space 1023
+	longitudClaveCorta: .word 0
+	
+	claveExtendida: .space 1024
 	
 	textoDeErrorDocumento: .asciiz "No se pudo leer el documento"
 
@@ -48,31 +51,15 @@
     		la $a1, mensaje
     		li $a2, 1024
     		syscall
+    		move $s0, $v0
+    		
+    		la $t1, longitudMensaje
+		sw $s0, 0($t1)
     		
     		#-------------------------- Cerrar archivo --------------------------#
     		li $v0, 16
         	move $a0, $t0
         	syscall
-        	
-        	#------------------- Contar cantidad de bytes -------------------#
-		la $t1, mensaje
-		li $t2, 0
-
-		contarWhile:
-			lb $t3, 0($t1)
-
-			addi $t1, $t1, 1
-			addi $t2, $t2, 1
-		
-			beqz $t3, endContarWhile
-		
-			j contarWhile
-		endContarWhile:
-		
-		sub $t2, $t2, 1
-		
-		la $t1, longitudMensaje
-		sw $t2, 0($t1)
         	
         jr $ra
         
@@ -87,27 +74,70 @@
 		la $a0, claveCorta
 		li $a1, 1023
 		syscall
+		
+		addi $sp, $sp, -4
+		sw $ra, 0($sp)
+    		
+    		jal ajustarClaveCorta
+    		
+    		lw $ra, 0($sp)
+		addi $sp, $sp, 4
+		
+		#--------------------------- Contar caracteres ---------------------------#
+		la $t1, claveCorta
+		li $t2, 0
+
+		contarWhile:
+			lb $t3, 0($t1)
+			
+			beqz $t3, endContarWhile
+
+			addi $t1, $t1, 1
+			addi $t2, $t2, 1
+		
+			j contarWhile
+		endContarWhile:
+		
+		la $t4, longitudClaveCorta
+		sw $t2, 0($t4)
         
+        jr $ra
+        
+        
+        ajustarClaveCorta:
+        
+		la $t0, claveCorta
+		
+		encontrarSalto:
+    			lb $t1, 0($t0)
+    			beqz $t1, eliminarSalto
+
+    			addi $t0, $t0, 1
+    			j encontrarSalto
+
+		eliminarSalto:
+    			addi $t0, $t0, -1
+    			lb $t1, 0($t0)
+    			li $t2, 10
+	
+    			beq $t1, $t2, reemplazar
+
+    			j salir
+
+		reemplazar:
+    			sb $zero, 0($t0)
+
+		salir:
+		
         jr $ra
         
         
         
         mostrarMensaje:
         
-        	la $t0, mensaje
-        	
-		mostrar_mensaje:
-    			lb $t1, 0($t0)
-    			beqz $t1, fin_mostrar_mensaje
-
-    			li $v0, 11
-    			move $a0, $t1
-    			syscall
-
-    			addi $t0, $t0, 1
-    			j mostrar_mensaje
-
-		fin_mostrar_mensaje:
+        	li $v0, 4
+		la $a0, mensaje
+		syscall
 		
 		lw $a0, longitudMensaje
 		li $v0, 1
@@ -115,6 +145,10 @@
 		
 		li $v0, 4
 		la $a0, claveCorta
+		syscall
+		
+		lw $a0, longitudClaveCorta
+		li $v0, 1
 		syscall
         	
     	jr $ra
